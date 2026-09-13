f8c9a8e90ddfb7a9a2dc39cf8901fb16b3f7c46a
moon18-eng <moo203417@gmail.com>
2026-09-13 23:21:43 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,42 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import pkg from "pg";
+import pool from "./src/config/db";
 
 
-dotenv.config();
-const { Pool } = pkg;
+dotenv.config(); //to read .env file
+
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-user: process.env.DB_USER,
-host: process.env.DB_HOST,
-database: process.env.DB_NAME,
-password: process.env.DB_PASS,
-port: process.env.DB_PORT,
-});
 
-
-// Run CREATE TABLE once when server starts
-const createTable = async () => {
+const createTables = async () => {
 try {
 await pool.query(`
-      CREATE TABLE IF NOT EXISTS notes (
+      CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
-        title VARCHAR(255),
-        content TEXT
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password_hash VARCHAR(255),
       );
     `);
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXIST dishes (
+        dish_name VARCHAR (255) UNIQUE NOT NULL,
+        dish_price INT,
+        dish _discription VARCHAR,
+        
+
+       
+      );
+    `);
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXIST orders (
+      );
+    `);
+
 
 console.log("Notes table created or already exists");
   } catch (err) {
@@ -38,8 +45,10 @@ console.error("Error creating table:", err.message);
 };
 
 
-createTable();
+createTables();
+
 app.get("/", (req, res) => res.send("Backend running!"));
+
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server started...");
 });
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,11 @@ import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-const { Pool } = pkg;
+const { Pool } = pkg; //creating a variable called pool, Pool is the db shema, pool is the real thing we built
 const pool = new Pool({
 connectionString: process.env.DATABASE_URL,
 ssl: {
-rejectUnauthorized: false, // required for Neon
+rejectUnauthorized: false, 
   },
 });
 
