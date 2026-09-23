280292bd2e55dfb495eec8b134977172b6c16890
moon18-eng <moo203417@gmail.com>
2026-09-23 18:57:53 +0100

    hey?

--- backend/server.js
+++ backend/server.js
@@ -13,27 +13,42 @@ app.use(express.json());
 
 const createTables = async () => {
 try {
-await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(255) UNIQUE NOT NULL,
-        password_hash VARCHAR(255),
-      );
-    `);
-
+ 
   await pool.query(`
       CREATE TABLE IF NOT EXIST dishes (
         id SERIAL PRIMARY KEY,
         dish_name VARCHAR (255) UNIQUE NOT NULL,
         dish_price INT,
         dish _discription VARCHAR,
+        is_available BOOLEAN DEFAULT true
+      );
+    `); //hide dish on false
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXIST orders (
+        id SERIAL PRIMARY KEY,
+        guest_name VARCHAR (255),
+        guest_phone INT,
+        guest_adress VARCHAR (255),
+        status VARCHAR(255) DEFAULT 'Pending,
+        Total_price INT,
+        ordered_at TIMESTAMP DEFAULT NOW()
       );
     `);
 
   await pool.query(`
-      CREATE TABLE IF NOT EXIST orders (
-      );
-    `);
+    
+    CREATE TABLE IF NOT EXIST order_items (
+      id SERIAL PRIMARY KEY,
+      order_id INT REFERENCE orders(id) ON DELETE CASCADE,
+      dish_id  INT REFERENCE dishes(id) ON DELETE RESTRICT,
+      order_price INT  REFERENCE dishes(dish_price) RESTRICT,
+      quantity INT NOT NULL
+    )
+    
+    `)
+
+  
 
 
 console.log("Notes table created or already exists");
